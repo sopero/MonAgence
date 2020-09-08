7a890f5f05111050d8f0d1afea039c6ccbdf260c
Nicolas Rondet <rondet.nicolas@gmail.com>
2020-09-08 17:16:16 +0200

    Add migrations

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,12 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PropertyController extends AbstractController
 {
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
 
     /**
      * @Route("/biens", name="property.index")
@@ -15,6 +29,10 @@ class PropertyController extends AbstractController
      */
     public function index(): Response
     {
+
+        $property = $this->repository->find(1);
+        dump($property);
+
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
         ]);
